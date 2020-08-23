34376e0ec22baebb94ea8e9c1f97627575f6824b
auriliosantos <auriliosantos@hotmail.com.br>
2020-08-23 08:36:59 -0300

    melhorias no tempo de resposta

--- build/src/controllers/InputsController.js
+++ build/src/controllers/InputsController.js
@@ -5,34 +5,7 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const connection_1 = __importDefault(require("../database/connection"));
 class InputsControler {
-    async insurers(request, response) {
-        try {
-            const insurersData = await connection_1.default("insurers").select("insurers.id", "insurers.name");
-            return response.json({ insurersData });
-        }
-        catch (err) {
-            return response.status(500).send();
-        }
-    }
-    async clients(request, response) {
-        try {
-            const clientsData = await connection_1.default("clients").select("clients.id", "clients.name");
-            return response.json({ clientsData });
-        }
-        catch (err) {
-            return response.status(500).send();
-        }
-    }
-    async usage_types(request, response) {
-        try {
-            const usage_typesData = await connection_1.default("usage_types").select("usage_types.id", "usage_types.name");
-            return response.json({ usage_typesData });
-        }
-        catch (err) {
-            return response.status(500).send();
-        }
-    }
-    async marital_statuses(request, response) {
+    async clientsData(request, response) {
         try {
             const marital_statusesData = await connection_1.default("marital_statuses").select("marital_statuses.id", "marital_statuses.name");
             return response.json({ marital_statusesData });
@@ -41,28 +14,28 @@ class InputsControler {
             return response.status(500).send();
         }
     }
-    async payment_methods(request, response) {
+    async vehiclesData(request, response) {
         try {
-            const payment_methodsData = await connection_1.default("payment_methods").select("payment_methods.id", "payment_methods.name");
-            return response.json({ payment_methodsData });
+            const ownerData = await connection_1.default("clients").select("clients.id", "clients.name");
+            const usage_typesData = await connection_1.default("usage_types").select("usage_types.id", "usage_types.name");
+            return response.json({ ownerData, usage_typesData });
         }
         catch (err) {
             return response.status(500).send();
         }
     }
-    async deductible_types(request, response) {
+    async insurancesData(request, response) {
         try {
-            const deductible_typesData = await connection_1.default("deductible_types").select("deductible_types.id", "deductible_types.name");
-            return response.json({ deductible_typesData });
-        }
-        catch (err) {
-            return response.status(500).send();
-        }
-    }
-    async vehicles(request, response) {
-        try {
+            const insurersData = await connection_1.default("insurers").select("insurers.id", "insurers.name");
             const vehiclesData = await connection_1.default("vehicles").select("vehicles.id", "vehicles.plate", "vehicles.chassis");
-            return response.json({ vehiclesData });
+            const deductible_typesData = await connection_1.default("deductible_types").select("deductible_types.id", "deductible_types.name");
+            const payment_methodsData = await connection_1.default("payment_methods").select("payment_methods.id", "payment_methods.name");
+            return response.json({
+                insurersData,
+                vehiclesData,
+                deductible_typesData,
+                payment_methodsData,
+            });
         }
         catch (err) {
             return response.status(500).send();
--- src/controllers/InputsController.ts
+++ src/controllers/InputsController.ts
@@ -3,43 +3,7 @@ import { Request, Response } from "express";
 import db from "../database/connection";
 
 export default class InputsControler {
-  async insurers(request: Request, response: Response) {
-    try {
-      const insurersData = await db("insurers").select(
-        "insurers.id",
-        "insurers.name"
-      );
-
-      return response.json({ insurersData });
-    } catch (err) {
-      return response.status(500).send();
-    }
-  }
-  async clients(request: Request, response: Response) {
-    try {
-      const clientsData = await db("clients").select(
-        "clients.id",
-        "clients.name"
-      );
-
-      return response.json({ clientsData });
-    } catch (err) {
-      return response.status(500).send();
-    }
-  }
-  async usage_types(request: Request, response: Response) {
-    try {
-      const usage_typesData = await db("usage_types").select(
-        "usage_types.id",
-        "usage_types.name"
-      );
-
-      return response.json({ usage_typesData });
-    } catch (err) {
-      return response.status(500).send();
-    }
-  }
-  async marital_statuses(request: Request, response: Response) {
+  async clientsData(request: Request, response: Response) {
     try {
       const marital_statusesData = await db("marital_statuses").select(
         "marital_statuses.id",
@@ -51,39 +15,48 @@ export default class InputsControler {
       return response.status(500).send();
     }
   }
-  async payment_methods(request: Request, response: Response) {
+  async vehiclesData(request: Request, response: Response) {
     try {
-      const payment_methodsData = await db("payment_methods").select(
-        "payment_methods.id",
-        "payment_methods.name"
+      const ownerData = await db("clients").select(
+        "clients.id",
+        "clients.name"
+      );
+      const usage_typesData = await db("usage_types").select(
+        "usage_types.id",
+        "usage_types.name"
       );
 
-      return response.json({ payment_methodsData });
+      return response.json({ ownerData, usage_typesData });
     } catch (err) {
       return response.status(500).send();
     }
   }
-  async deductible_types(request: Request, response: Response) {
+  async insurancesData(request: Request, response: Response) {
     try {
-      const deductible_typesData = await db("deductible_types").select(
-        "deductible_types.id",
-        "deductible_types.name"
+      const insurersData = await db("insurers").select(
+        "insurers.id",
+        "insurers.name"
       );
-
-      return response.json({ deductible_typesData });
-    } catch (err) {
-      return response.status(500).send();
-    }
-  }
-  async vehicles(request: Request, response: Response) {
-    try {
       const vehiclesData = await db("vehicles").select(
         "vehicles.id",
         "vehicles.plate",
         "vehicles.chassis"
       );
+      const deductible_typesData = await db("deductible_types").select(
+        "deductible_types.id",
+        "deductible_types.name"
+      );
+      const payment_methodsData = await db("payment_methods").select(
+        "payment_methods.id",
+        "payment_methods.name"
+      );
 
-      return response.json({ vehiclesData });
+      return response.json({
+        insurersData,
+        vehiclesData,
+        deductible_typesData,
+        payment_methodsData,
+      });
     } catch (err) {
       return response.status(500).send();
     }
